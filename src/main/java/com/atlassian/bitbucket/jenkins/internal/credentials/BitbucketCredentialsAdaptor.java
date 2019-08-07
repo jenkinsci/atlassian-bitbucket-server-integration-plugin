fb461b3c21e2744c9fd2a3db5eb465eaa17513e8
goravjoshi <gaurav.joshi@live.com>
2019-08-07 14:39:52 +1000

    incorporating review comments.

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/credentials/BitbucketCredentialsAdaptor.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/credentials/BitbucketCredentialsAdaptor.java
@@ -16,13 +16,12 @@ import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
 
-public class BitbucketCredentialsAdaptor implements BitbucketCredentials {
+public final class BitbucketCredentialsAdaptor implements BitbucketCredentials {
 
     private final Credentials credentials;
 
-    private BitbucketCredentialsAdaptor(@Nonnull Credentials credentials) {
-        requireNonNull(credentials);
-        this.credentials = credentials;
+    private BitbucketCredentialsAdaptor(Credentials credentials) {
+        this.credentials = requireNonNull(credentials);
     }
 
     public static BitbucketCredentials createWithFallback(@Nullable String credentials,
@@ -41,7 +40,6 @@ public class BitbucketCredentialsAdaptor implements BitbucketCredentials {
     static BitbucketCredentials create(@Nonnull Credentials credentials) {
         return new BitbucketCredentialsAdaptor(credentials);
     }
-
 
     @Override
     public String toHeaderValue() {
@@ -64,8 +62,10 @@ public class BitbucketCredentialsAdaptor implements BitbucketCredentials {
     }
 
     private static BitbucketCredentials create(BitbucketServerConfiguration configuration) {
-        return Optional.ofNullable(configuration.getCredentials())
-                .map(credentials -> (BitbucketCredentials) new BitbucketCredentialsAdaptor(credentials))
-                .orElse(ANONYMOUS_CREDENTIALS);
+        if (configuration.getCredentials() != null) {
+            return new BitbucketCredentialsAdaptor(configuration.getCredentials());
+        } else {
+            return ANONYMOUS_CREDENTIALS;
+        }
     }
 }
