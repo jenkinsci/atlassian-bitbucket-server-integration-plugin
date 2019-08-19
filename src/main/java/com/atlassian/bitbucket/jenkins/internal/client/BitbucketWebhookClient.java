0162c9614c4ed86e705d4e37c40e78b4dfc4fc60
goravjoshi <gaurav.joshi@live.com>
2019-08-19 10:22:11 +1000

    Incorporating review comments

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/client/BitbucketWebhookClient.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/client/BitbucketWebhookClient.java
@@ -2,20 +2,21 @@ package com.atlassian.bitbucket.jenkins.internal.client;
 
 import com.atlassian.bitbucket.jenkins.internal.model.BitbucketPage;
 import com.atlassian.bitbucket.jenkins.internal.model.BitbucketWebhook;
+import com.atlassian.bitbucket.jenkins.internal.model.BitbucketWebhookRequest;
 
 /**
- * A client to query for and register web hooks in Bitbucket Server.
+ * A client to query for and register webhooks in Bitbucket Server.
  */
 public interface BitbucketWebhookClient {
 
     /**
      * Fetch existing webhooks. Result could be further filtered by passing in event id filters.
      *
-     * @param eventIdFilter, Event id filters. These ids are the same as the one recieved as
+     * @param eventId, Event id filters. These ids are the same as the one recieved as
      *                       {@link com.atlassian.bitbucket.jenkins.internal.model.BitbucketWebhookSupportedEvents}
      * @return a page of webhooks.
      */
-    BitbucketPage<BitbucketWebhook> getWebhooks(String... eventIdFilter);
+    BitbucketPage<BitbucketWebhook> getWebhooks(String... eventId);
 
     /**
      * Registers the given webhook in the Bitbucket Server.
@@ -23,5 +24,5 @@ public interface BitbucketWebhookClient {
      * @param request Webhook details
      * @return returns the registered webhook
      */
-    BitbucketWebhook registerWebhook(WebhookRegisterRequest request);
+    BitbucketWebhook registerWebhook(BitbucketWebhookRequest request);
 }
