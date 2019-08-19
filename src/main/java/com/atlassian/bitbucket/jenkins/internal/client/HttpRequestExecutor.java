28f8721cd1cef9796517d05441c8ee02174c0752
goravjoshi <gaurav.joshi@live.com>
2019-08-19 10:22:11 +1000

    Incorporating review comments

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/client/HttpRequestExecutor.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/client/HttpRequestExecutor.java
@@ -8,17 +8,16 @@ import okhttp3.Response;
  * Responsible for making remote HTTP calls to the given URL using passed in credentials. The implementation is tightly
  * bound with OkHttpClient library. Methods also takes {@link ResponseConsumer} instead of returning response in order
  * to have better handle on cleaning of resources.
- *
  */
 public interface HttpRequestExecutor {
 
     /**
      * Executes a Get call to a given URL.
      *
-     * @param url,        The URL to hit on bitbucket server end.
-     * @param credential, Credentials that will be used in making calls
-     * @param consumer,   on successful execution, {@link Response} will be passed to consumer.
-     * @param <T>,        result that consumer wish to return.
+     * @param url        The URL to hit on bitbucket server end.
+     * @param credential Credentials that will be used in making calls
+     * @param consumer   on successful execution, {@link Response} will be passed to consumer.
+     * @param <T>        result that consumer wish to return.
      * @throws AuthorizationException     if the credentials did not allow access to the given url
      * @throws ConnectionFailureException if the server did not respond
      * @throws NotFoundException          if the requested url does not exist
@@ -31,23 +30,25 @@ public interface HttpRequestExecutor {
     /**
      * Executes a POST with a given URL and request payload.
      *
-     * @param url, The URL to hit on bitbucket server end.
-     * @param credentials, Credentials that will be used in making calls
-     * @param requestBody, the request payload to send
-     * @param consumer, on successful execution, {@link Response} will be passed to consumer.
-     * @param <T>, result that consumer wish to return.
-     * @return, result computed by consumer.
+     * @param url               The URL to hit on bitbucket server end.
+     * @param credentials       Credentials that will be used in making calls
+     * @param requestBodyAsJson the request payload to send in JSON format
+     * @param consumer          on successful execution, {@link Response} will be passed to consumer.
+     * @param <T>               result that consumer wish to return.
+     * @return result computed by consumer.
      * @throws AuthorizationException     if the credentials did not allow access to the given url
      * @throws ConnectionFailureException if the server did not respond
      * @throws NotFoundException          if the requested url does not exist
      * @throws BadRequestException        if the request was malformed and thus rejected by the server
      * @throws ServerErrorException       if the server failed to process the request
      * @throws BitbucketClientException   for all errors not already captured
-     *
      */
-    <T> T executePost(HttpUrl url, BitbucketCredentials credentials, String requestBody, ResponseConsumer<T> consumer);
+    <T> T executePost(HttpUrl url, BitbucketCredentials credentials, String requestBodyAsJson,
+                      ResponseConsumer<T> consumer);
 
     interface ResponseConsumer<T> {
+
+        ResponseConsumer<Void> EMPTY_RESPONSE = response -> null;
 
         T consume(Response response);
     }
--- src/main/java/com/atlassian/bitbucket/jenkins/internal/model/BitbucketWebhook.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/model/BitbucketWebhook.java
@@ -3,9 +3,8 @@ package com.atlassian.bitbucket.jenkins.internal.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 
+import java.util.Objects;
 import java.util.Set;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -37,16 +36,11 @@ public class BitbucketWebhook extends BitbucketWebhookRequest {
             return false;
         }
         BitbucketWebhook that = (BitbucketWebhook) o;
-
-        return new EqualsBuilder()
-                .append(id, that.id)
-                .isEquals();
+        return id == that.id;
     }
 
     @Override
     public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-                .append(id)
-                .toHashCode();
+        return Objects.hash(id);
     }
 }
