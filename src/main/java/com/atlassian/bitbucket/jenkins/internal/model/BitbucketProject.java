725fd0878ea012f803bf70117f11998bf9f50817
Kristy Hughes <khughes@atlassian.com>
2019-09-16 16:40:25 +1000

    Improve project and repo config page by removing key and slug from the form

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/model/BitbucketProject.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/model/BitbucketProject.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+import java.util.Map;
+
 import static java.util.Objects.requireNonNull;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -11,13 +14,19 @@ public class BitbucketProject {
 
     private final String key;
     private final String name;
+    private String selfLink;
 
     @JsonCreator
     public BitbucketProject(
             @JsonProperty(value = "key", required = true) String key,
+            @JsonProperty("links") Map<String, List<BitbucketNamedLink>> links,
             @JsonProperty(value = "name", required = true) String name) {
         this.key = requireNonNull(key, "key");
         this.name = requireNonNull(name, "name");
+        List<BitbucketNamedLink> link = requireNonNull(links, "links").get("self");
+        if (link != null && !link.isEmpty()) { // there should always be exactly one self link.
+            selfLink = link.get(0).getHref();
+        }
     }
 
     public String getKey() {
@@ -26,5 +35,9 @@ public class BitbucketProject {
 
     public String getName() {
         return name;
+    }
+
+    public String getSelfLink() {
+        return selfLink;
     }
 }
