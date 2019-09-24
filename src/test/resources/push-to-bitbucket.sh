4c90405bfc22b39f442ac728ea30f39cf17ac0a8
Martin Henschke <mhenschke@atlassian.com>
2019-09-24 12:05:26 +1000

    Testing bash version

--- src/test/resources/push-to-bitbucket.sh
+++ src/test/resources/push-to-bitbucket.sh
@@ -1,6 +1,5 @@
 git checkout master
-echo git remote get-url origin
-git remote set-url origin http://localhost:7990/bitbucket/scm/project_1/rep_1.git
+echo bash --version:w
 echo "Hello, World!" >> test.txt
 git add test.txt
 git commit -m "uniqueMessage"
