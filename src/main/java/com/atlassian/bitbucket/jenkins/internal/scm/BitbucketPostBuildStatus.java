6f1ec56e29e5c8110ab2821ddcf7c82b03fe9d82
dkjellin <48895750+dkjellin@users.noreply.github.com>
2020-04-09 12:49:52 +1000

    Dk/rich build status add branch (#139)
    * Add branch to rich build status
    
    * Fix bug with validation, use correct revision for posting

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketPostBuildStatus.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketPostBuildStatus.java
@@ -9,9 +9,10 @@ import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.plugins.git.GitException;
 import hudson.plugins.git.GitSCM;
+import hudson.plugins.git.Revision;
 import hudson.plugins.git.extensions.GitSCMExtension;
 import hudson.plugins.git.util.BuildData;
-import org.jenkinsci.plugins.gitclient.CheckoutCommand;
+import org.apache.commons.lang3.StringUtils;
 import org.jenkinsci.plugins.gitclient.GitClient;
 
 class BitbucketPostBuildStatus extends GitSCMExtension {
@@ -29,26 +30,37 @@ class BitbucketPostBuildStatus extends GitSCMExtension {
     }
 
     @Override
-    public void decorateCheckoutCommand(GitSCM scm, Run<?, ?> run, GitClient git, TaskListener listener,
-                                        CheckoutCommand cmd) throws GitException {
+    public Revision decorateRevisionToBuild(GitSCM scm, Run<?, ?> run, GitClient git, TaskListener listener, Revision marked, Revision rev) throws GitException {
         Injector injector = jenkinsProvider.get().getInjector();
         if (injector == null) {
             listener.getLogger().println("Injector could not be found while creating build status");
-            return;
+            return rev;
         }
 
         BuildData buildData = scm.getBuildData(run);
+
+        String ref = rev.getBranches().stream()
+                .map(branch -> {
+                    String name = branch.getName();
+                    if (StringUtils.startsWith(name, "refs/")) {
+                        return name;
+                    }
+                    return "refs/heads/" + name;
+                })
+                .findFirst()
+                .orElse(null);
         if (buildData == null || buildData.lastBuild == null) {
-            listener.getLogger().println("Build data was not found while creating a build status");
-            return;
+            run.addAction(new BitbucketRevisionAction(ref, rev.getSha1String(), serverId));
+        } else {
+            run.addAction(new BitbucketRevisionAction(ref, buildData.lastBuild.getRevision().getSha1String(), serverId));
         }
-        String revisionSha1 = buildData.lastBuild.getRevision().getSha1String();
-        run.addAction(new BitbucketRevisionAction(revisionSha1, serverId));
         BuildStatusPoster poster = injector.getInstance(BuildStatusPoster.class);
         if (poster != null) {
             poster.postBuildStatus(run, listener);
         } else {
             listener.getLogger().println("Build Status Poster instance could not be found while creating a build status");
         }
+
+        return rev;
     }
 }
--- src/main/java/com/atlassian/bitbucket/jenkins/internal/status/BitbucketRevisionAction.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/status/BitbucketRevisionAction.java
@@ -3,13 +3,16 @@ package com.atlassian.bitbucket.jenkins.internal.status;
 import hudson.model.Action;
 
 import javax.annotation.CheckForNull;
+import javax.annotation.Nullable;
 
 public class BitbucketRevisionAction implements Action {
 
+    private final String ref;
     private final String revisionSha1;
     private final String serverId;
 
-    public BitbucketRevisionAction(String revisionSha1, String serverId) {
+    public BitbucketRevisionAction(@Nullable String ref, String revisionSha1, String serverId) {
+        this.ref = ref;
         this.revisionSha1 = revisionSha1;
         this.serverId = serverId;
     }
@@ -24,6 +27,11 @@ public class BitbucketRevisionAction implements Action {
     @Override
     public String getIconFileName() {
         return null;
+    }
+
+    @CheckForNull
+    public String getRefName() {
+        return ref;
     }
 
     public String getRevisionSha1() {
