03c0afade437ab2b061bfebcac57c21670464c15
Ali Kord <akord@atlassian.com>
2020-07-20 11:55:30 +1000

    ignored breaking test

--- acceptance-tests/src/test/java/it/com/atlassian/bitbucket/jenkins/internal/SmokeTest.java
+++ acceptance-tests/src/test/java/it/com/atlassian/bitbucket/jenkins/internal/SmokeTest.java
@@ -184,6 +184,7 @@ public class SmokeTest extends AbstractJUnitTest {
     }
 
     @Test
+    @Ignore("https://issues.jenkins-ci.org/browse/JENKINS-63121")
     public void testFullBuildFlowWithMultiBranchJobAndBitbucketWebhookTrigger() throws IOException, GitAPIException {
         BitbucketScmWorkflowMultiBranchJob multiBranchJob =
                 jenkins.jobs.create(BitbucketScmWorkflowMultiBranchJob.class);
