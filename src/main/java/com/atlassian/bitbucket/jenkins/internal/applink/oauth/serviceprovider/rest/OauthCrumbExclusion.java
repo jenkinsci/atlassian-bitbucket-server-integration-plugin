6e181d6a6fc9c4046a966fc2874282b946e7639b
Daniel Kjellin <dkjellin@atlassian.com>
2020-08-24 11:23:43 +1000

    Add crumb exception for Oauth (applink) requests as they should be unprotected and have proper handling for these things already

---
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/applink/oauth/serviceprovider/rest/OauthCrumbExclusion.java
@@ -0,0 +1,31 @@
+package com.atlassian.bitbucket.jenkins.internal.applink.oauth.serviceprovider.rest;
+
+import com.atlassian.bitbucket.jenkins.internal.applink.oauth.serviceprovider.OAuthRequestUtils;
+import hudson.Extension;
+import hudson.security.csrf.CrumbExclusion;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
+/**
+ * Adds exception to the CSRF protection filter for OAuth (applink) requests.
+ */
+@Extension
+public class OauthCrumbExclusion extends CrumbExclusion {
+
+    @Override
+    public boolean process(HttpServletRequest req, HttpServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+        String pathInfo = req.getPathInfo();
+        if (isEmpty(pathInfo) || !OAuthRequestUtils.isOAuthAccessAttempt(req)) {
+            return false;
+        }
+        chain.doFilter(req, resp);
+        return true;
+    }
+}
