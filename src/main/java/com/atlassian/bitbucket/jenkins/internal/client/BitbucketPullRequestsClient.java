37d0f52b8abd74efd0c78fda4f2b3a618e0eafca
ali3-atlassian <74999950+ali3-atlassian@users.noreply.github.com>
2021-01-22 15:12:38 +1100

    Update src/main/java/com/atlassian/bitbucket/jenkins/internal/client/BitbucketPullRequestsClient.java
    Co-authored-by: dkjellin <48895750+dkjellin@users.noreply.github.com>

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/client/BitbucketPullRequestsClient.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/client/BitbucketPullRequestsClient.java
@@ -9,5 +9,13 @@ import java.util.stream.Stream;
  */
 public interface BitbucketPullRequestsClient {
 
+    /**
+     * Gets all the open pull requests for the repository. The returned stream will make paged calls to Bitbucket to
+     * ensure that all pull requests are returned. Consumers are advised that this can return large amounts of data
+     * and are <strong>strongly</strong> encouraged to not collect to a list or similar before processing items, but
+     * rather process them as they come in.
+     *
+     * @return a stream of all (potentially spanning multiple pages) open pull requests
+     */
     Stream<BitbucketPullRequest> getOpenPullRequests();
 }
