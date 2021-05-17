74c6212f3110f8a994724332ec7d23571bbc45e3
Martin Henschke <mhenschke@atlassian.com>
2021-05-17 15:02:55 +1000

    Add crumb information to oauth consumer post request

--- acceptance-tests/src/main/java/it/com/atlassian/bitbucket/jenkins/internal/applink/oauth/client/JenkinsApplinksClient.java
+++ acceptance-tests/src/main/java/it/com/atlassian/bitbucket/jenkins/internal/applink/oauth/client/JenkinsApplinksClient.java
@@ -3,6 +3,8 @@ package it.com.atlassian.bitbucket.jenkins.internal.applink.oauth.client;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.response.Response;
 import it.com.atlassian.bitbucket.jenkins.internal.applink.oauth.model.OAuthConsumer;
 
 import static io.restassured.http.ContentType.URLENC;
@@ -25,10 +27,20 @@ public class JenkinsApplinksClient {
     public OAuthConsumer createOAuthConsumer() throws JsonProcessingException {
         OAuthConsumer consumer = newConsumer();
         String consumerCreateUri = absoluteUrl(CREATE_OAUTH_CONSUMER_PATH);
+
+        // TODO: Create in a static helper, make more consumable
+        Response response = RestAssured.given()
+                .get(absoluteUrl("/crumbIssuer/api/xml"));
+
+        JenkinsCrumb crumb = response.getBody().as(JenkinsCrumb.class);
+        String cookies = response.getHeader("Set-Cookie");
+
         RestAssured.given()
                 .contentType(URLENC)
                 .formParam("json", jsonSerializer.writeValueAsString(consumer))
                 .when()
+                .header("Cookie", cookies)
+                .header(crumb.getCrumbHeader())
                 .post(consumerCreateUri)
                 .then()
                 .statusCode(302);
