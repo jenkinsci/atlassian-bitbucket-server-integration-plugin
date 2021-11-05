ac3cdd26aedfde07c9430abc93e852c44ad063d1
Martin Henschke <mhenschke@atlassian.com>
2021-11-05 14:50:20 +1100

    Remove nonnull

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketSCMSource.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketSCMSource.java
@@ -17,7 +17,6 @@ import com.atlassian.bitbucket.jenkins.internal.trigger.events.AbstractWebhookEv
 import com.atlassian.bitbucket.jenkins.internal.trigger.register.WebhookRegistrationFailed;
 import com.cloudbees.hudson.plugins.folder.computed.ComputedFolder;
 
-import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.Extension;
 import hudson.model.Action;
@@ -161,10 +160,9 @@ public class BitbucketSCMSource extends SCMSource {
         return getGitSCMSource().build(head, revision);
     }
     
-    @NonNull
     @Override
-    protected List<Action> retrieveActions(SCMSourceEvent event,
-                                          @NonNull TaskListener listener) throws IOException, InterruptedException {
+    protected List<Action> retrieveActions(SCMSourceEvent event, 
+                                           TaskListener listener) throws IOException, InterruptedException {
         List<Action> result = new ArrayList<>();
         BitbucketSCMSource.DescriptorImpl descriptor = (BitbucketSCMSource.DescriptorImpl) getDescriptor();
         Optional<BitbucketServerConfiguration> mayBeServerConf = descriptor.getConfiguration(getServerId());
@@ -187,11 +185,10 @@ public class BitbucketSCMSource extends SCMSource {
         return result;
     }
 
-    @NonNull
     @Override
-    protected List<Action> retrieveActions(@NonNull SCMHead head, 
+    protected List<Action> retrieveActions(SCMHead head, 
                                            @CheckForNull SCMHeadEvent event,
-                                           @NonNull TaskListener listener) throws IOException, InterruptedException {       
+                                           TaskListener listener) throws IOException, InterruptedException {       
         List<Action> result = new ArrayList<>();
         SCMSourceOwner owner = getOwner();
         if (owner instanceof Actionable) {
