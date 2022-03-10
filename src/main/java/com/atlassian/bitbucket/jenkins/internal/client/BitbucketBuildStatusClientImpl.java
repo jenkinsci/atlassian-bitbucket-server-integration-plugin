81245e047e057a12b8acee17c9e07b37cc64d476
Daniel Kjellin <dkjellin@atlassian.com>
2022-03-10 14:05:32 +1100

    JENKINS-66539 move sysprop name to constants file

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/client/BitbucketBuildStatusClientImpl.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/client/BitbucketBuildStatusClientImpl.java
@@ -4,13 +4,14 @@ import com.atlassian.bitbucket.jenkins.internal.http.RetryOnRateLimitConfig;
 import com.atlassian.bitbucket.jenkins.internal.model.BitbucketBuildStatus;
 import okhttp3.HttpUrl;
 
+import static com.atlassian.bitbucket.jenkins.internal.util.SystemPropertiesConstants.REQUEST_RETRY_MAX_ATTEMPTS;
 import static java.util.Objects.requireNonNull;
 import static org.apache.commons.lang3.StringUtils.stripToNull;
 
 public class BitbucketBuildStatusClientImpl implements BitbucketBuildStatusClient {
 
     private static final String BUILD_STATUS_VERSION = "1.0";
-    private static final int maxAttempts = Integer.parseInt(System.getProperty("bitbucket.retry.request.attempts", "3"));
+    private static final int maxAttempts = Integer.parseInt(System.getProperty(REQUEST_RETRY_MAX_ATTEMPTS, "3"));
     private final BitbucketRequestExecutor bitbucketRequestExecutor;
     private final String revisionSha;
 
