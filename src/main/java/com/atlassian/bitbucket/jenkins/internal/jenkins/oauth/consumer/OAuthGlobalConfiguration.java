067f41c638a0e0f26b5de2718cc5b2d3b43e43ee
NotMyFault <mc.cache@web.de>
2022-04-14 09:51:04 +0200

    fix: Adapt icon path removal from core

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/jenkins/oauth/consumer/OAuthGlobalConfiguration.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/jenkins/oauth/consumer/OAuthGlobalConfiguration.java
@@ -82,7 +82,7 @@ public class OAuthGlobalConfiguration extends ManagementLink implements Describa
     @CheckForNull
     @Override
     public String getIconFileName() {
-        return "secure.gif";
+        return "secure.png";
     }
 
     @CheckForNull
