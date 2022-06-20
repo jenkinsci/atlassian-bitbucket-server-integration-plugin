6913a9bd666349a2df7e88f349378a589de51ff0
Kristy <khughes@atlassian.com>
2022-06-20 14:40:52 +1000

    Bugfix: JENKINS-68634 Clone from mirror clone URL when configured to clone from a mirror (#347)
    * Add test for initializeScm
    
    * Clone from mirror clone URL when configured to clone from a mirror

--- src/test/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketSCMTest.java
+++ src/test/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketSCMTest.java
@@ -3,13 +3,15 @@ package com.atlassian.bitbucket.jenkins.internal.scm;
 import com.atlassian.bitbucket.jenkins.internal.config.BitbucketServerConfiguration;
 import com.atlassian.bitbucket.jenkins.internal.config.BitbucketTokenCredentials;
 import com.atlassian.bitbucket.jenkins.internal.credentials.GlobalCredentialsProvider;
+import com.atlassian.bitbucket.jenkins.internal.model.*;
+import hudson.model.Item;
 import hudson.scm.SCMDescriptor;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
-import java.util.Collections;
 import java.util.Optional;
 
+import static java.util.Collections.*;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -83,6 +85,69 @@ public class BitbucketSCMTest {
         assertEquals(projectName, scm.getProjectName());
     }
 
+    @Test
+    public void testInitializeScm() {
+        String credentialsId = "valid-credentials";
+        String serverId = "serverId1";
+        String projectName = "Project 1";
+        String projectKey = "PROJECT_1";
+        String repoName = "Repository 1";
+        String repoSlug = "rep_1";
+        String cloneUrl = "http://upstream";
+
+        BitbucketSCM.DescriptorImpl descriptor = mockDescriptor();
+        mockFetchedRepository(descriptor, projectName, projectKey, repoName, repoSlug, cloneUrl, "", "");
+        BitbucketSCM scm = spy(new BitbucketSCM("1", emptyList(), credentialsId, "", emptyList(), "", projectName,
+                repoName, serverId, "") {
+            @Override
+            public SCMDescriptor<?> getDescriptor() {
+                return descriptor;
+            }
+        });
+
+        BitbucketSCMRepository scmRepository = new BitbucketSCMRepository(credentialsId, null, projectName,
+                projectKey, repoName, repoSlug, serverId, "");
+        doReturn(scmRepository).when(scm).getBitbucketSCMRepository();
+
+        Item item = mock(Item.class);
+        scm.getAndInitializeGitScmIfNull(item);
+
+        assertEquals(cloneUrl, scm.getGitSCM().getUserRemoteConfigs().get(0).getUrl());
+    }
+
+    @Test
+    public void testInitializeScmForMirror() {
+        String credentialsId = "valid-credentials";
+        String serverId = "serverId1";
+        String projectName = "Project 1";
+        String projectKey = "PROJECT_1";
+        String repoName = "Repository 1";
+        String repoSlug = "rep_1";
+        String mirrorName = "myMirror";
+        String upstreamUrl = "http://upstream";
+        String mirrorUrl = "http://mirror";
+
+        BitbucketSCM.DescriptorImpl descriptor = mockDescriptor();
+        mockFetchedRepository(descriptor, projectName, projectKey, repoName, repoSlug, upstreamUrl, mirrorName, mirrorUrl);
+
+        BitbucketSCM scm = spy(new BitbucketSCM("1", emptyList(), credentialsId, "", emptyList(), "", projectName,
+                repoName, serverId, mirrorName) {
+            @Override
+            public SCMDescriptor<?> getDescriptor() {
+                return descriptor;
+            }
+        });
+
+        BitbucketSCMRepository scmRepository = new BitbucketSCMRepository(credentialsId, null, projectName,
+                projectKey, repoName, repoSlug, serverId, mirrorName);
+        doReturn(scmRepository).when(scm).getBitbucketSCMRepository();
+
+        Item item = mock(Item.class);
+        scm.getAndInitializeGitScmIfNull(item);
+
+        assertEquals(mirrorUrl, scm.getGitSCM().getUserRemoteConfigs().get(0).getUrl());
+    }
+
     private BitbucketSCM createInstance(String credentialId) {
         return createInstance(credentialId, null);
     }
@@ -103,10 +168,10 @@ public class BitbucketSCMTest {
                                         String mirror) {
         return new BitbucketSCM(
                 "1",
-                Collections.emptyList(),
+                emptyList(),
                 credentialsId,
                 "",
-                Collections.emptyList(),
+                emptyList(),
                 "",
                 project,
                 repo,
@@ -114,20 +179,41 @@ public class BitbucketSCMTest {
                 mirror) {
             @Override
             public SCMDescriptor<?> getDescriptor() {
-                BitbucketServerConfiguration bitbucketServerConfiguration = mock(BitbucketServerConfiguration.class);
-                doReturn(mock(GlobalCredentialsProvider.class))
-                        .when(bitbucketServerConfiguration).getGlobalCredentialsProvider(any(String.class));
-                DescriptorImpl descriptor = mock(DescriptorImpl.class);
-                when(descriptor.getConfiguration(argThat(serverId -> !isBlank(serverId))))
-                        .thenReturn(Optional.of(bitbucketServerConfiguration));
-                when(descriptor.getConfiguration(argThat(StringUtils::isBlank)))
-                        .thenReturn(Optional.empty());
-                when(descriptor.getBitbucketScmHelper(
-                        nullable(String.class),
-                        nullable(BitbucketTokenCredentials.class)))
-                        .thenReturn(mock(BitbucketScmHelper.class));
-                return descriptor;
+                return mockDescriptor();
             }
         };
+    }
+
+    private BitbucketSCM.DescriptorImpl mockDescriptor() {
+        BitbucketServerConfiguration bitbucketServerConfiguration = mock(BitbucketServerConfiguration.class);
+        doReturn(mock(GlobalCredentialsProvider.class))
+                .when(bitbucketServerConfiguration).getGlobalCredentialsProvider(any(String.class));
+        BitbucketSCM.DescriptorImpl descriptor = mock(BitbucketSCM.DescriptorImpl.class);
+        when(descriptor.getConfiguration(argThat(serverId -> !isBlank(serverId))))
+                .thenReturn(Optional.of(bitbucketServerConfiguration));
+        when(descriptor.getConfiguration(argThat(StringUtils::isBlank)))
+                .thenReturn(Optional.empty());
+        when(descriptor.getBitbucketScmHelper(
+                nullable(String.class),
+                nullable(BitbucketTokenCredentials.class)))
+                .thenReturn(mock(BitbucketScmHelper.class));
+        return descriptor;
+    }
+
+    private void mockFetchedRepository(BitbucketSCM.DescriptorImpl descriptor, String projectName, String projectKey,
+                                       String repoName, String repoSlug, String upstreamUrl,
+                                       String mirrorName, String mirrorUrl) {
+        BitbucketProject bitbucketProject = new BitbucketProject(projectKey, emptyMap(), projectName);
+        BitbucketRepository bitbucketRepository = new BitbucketRepository(1, repoName, bitbucketProject, repoSlug,
+                RepositoryState.AVAILABLE, singletonList(new BitbucketNamedLink("http", upstreamUrl)), "");
+        BitbucketScmHelper scmHelper = descriptor.getBitbucketScmHelper(null, null);
+        when(scmHelper.getRepository(projectName, repoName)).thenReturn(bitbucketRepository);
+        BitbucketMirroredRepository mirroringDetails = new BitbucketMirroredRepository(true, singletonMap("clone",
+                singletonList(new BitbucketNamedLink("http", mirrorUrl))), mirrorName, 1,
+                BitbucketMirroredRepositoryStatus.AVAILABLE);
+        BitbucketMirrorHandler mirrorHandler = mock(BitbucketMirrorHandler.class);
+        EnrichedBitbucketMirroredRepository mirroredRepository = new EnrichedBitbucketMirroredRepository(bitbucketRepository, mirroringDetails);
+        when(mirrorHandler.fetchRepository(any())).thenReturn(mirroredRepository);
+        when(descriptor.createMirrorHandler(any())).thenReturn(mirrorHandler);
     }
 }
