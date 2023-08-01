224719b8efb27ae6f1d1084a791e8d44556b453e
Dyon Georgopoulos <dgeorgopoulos@atlassian.com>
2023-08-01 12:47:48 +0930

    update javadoc for branch client get remote branches

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/client/BitbucketBranchClient.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/client/BitbucketBranchClient.java
@@ -7,11 +7,12 @@ import java.util.stream.Stream;
 public interface BitbucketBranchClient {
 
     /**
-     * Hits the bitbucket 'find branches' REST endpoint.
-     * Retrieve the branches matching the supplied filterText param.
-     * Default filterTest is empty. Which gets all branches.
-     * The authenticated user must have REPO_READ permission for the specified repository to call this resource.
-     * @return
+     * Gets all branches from the given repository.
+     * The returned stream will make paged calls to Bitbucket to ensure that all branches are returned.
+     * Consumers are advised that this can return large amounts of data and are <strong>strongly</strong> encouraged to
+     * not collect to a list or similar before processing items, but rather process them as they come in.
+     * 
+     * @return Stream of bitbucket branches
      */
     Stream<BitbucketDefaultBranch> getRemoteBranches();
 }
