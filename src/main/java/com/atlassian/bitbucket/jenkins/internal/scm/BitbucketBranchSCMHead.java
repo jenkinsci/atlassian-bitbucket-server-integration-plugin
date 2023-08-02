9ada39bbb1f15812341c940c0919818e587296fd
Josh Aguilar <jaguilar3@atlassian.com>
2023-08-02 16:31:14 +1200

    Fix incorrect branch head in PR trigger

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketBranchSCMHead.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketBranchSCMHead.java
@@ -1,6 +1,7 @@
 package com.atlassian.bitbucket.jenkins.internal.scm;
 
 import com.atlassian.bitbucket.jenkins.internal.model.BitbucketDefaultBranch;
+import com.atlassian.bitbucket.jenkins.internal.model.BitbucketPullRequestRef;
 import com.atlassian.bitbucket.jenkins.internal.model.BitbucketRefChange;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
@@ -12,16 +13,22 @@ import jenkins.scm.api.SCMRevision;
 
 public class BitbucketBranchSCMHead extends BitbucketSCMHead {
 
+    private static final long UNKNOWN_TIMESTAMP = -1;
+
     public BitbucketBranchSCMHead(String name) {
-        super(name, null, -1);
+        super(name, null, UNKNOWN_TIMESTAMP);
     }
 
     public BitbucketBranchSCMHead(BitbucketDefaultBranch branch) {
-        super(branch.getDisplayId(), branch.getLatestCommit(), -1);
+        super(branch.getDisplayId(), branch.getLatestCommit(), UNKNOWN_TIMESTAMP);
+    }
+
+    public BitbucketBranchSCMHead(BitbucketPullRequestRef prRef) {
+        super(prRef.getDisplayId(), prRef.getLatestCommit(), UNKNOWN_TIMESTAMP);
     }
 
     public BitbucketBranchSCMHead(BitbucketRefChange refChange) {
-        super(refChange.getRef().getDisplayId(), refChange.getToHash(), -1);
+        super(refChange.getRef().getDisplayId(), refChange.getToHash(), UNKNOWN_TIMESTAMP);
     }
 
     /**
